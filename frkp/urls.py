f24fa238d0cae7f384ec31d23946c84dd8139090
rufokman <rufina.tulegenova@gmail.com>
2022-05-31 20:13:53 +0400

    update models

--- frkp/urls.py
+++ frkp/urls.py
@@ -20,5 +20,5 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('frkpapp.urls'))
+    path('frkp/', include('frkpapp.urls'))
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- frkpapp/views.py
+++ frkpapp/views.py
@@ -1,18 +1,38 @@
-from django.http import HttpResponse
-from django.shortcuts import render
-
+from django.shortcuts import get_object_or_404
+from django.urls import reverse_lazy
+from django.views.generic import ListView, TemplateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .forms import *
 
 
-def parameters_view(request):
-    form = AddPostForm(request.POST or None)
-    if form.is_valid():
-        print('form valid')
-        form.save()
-    else:
-        print(form.errors)
-    context = {
-        'form': form
-    }
+class ParametersCreateView(CreateView):
+    form_class = AddPostForm
+    template_name = "frkpapp/param.html"
+    success_url = 'history/'
 
-    return render(request, 'frkpapp/param.html', context)
+
+class HistoryView(ListView):
+    model = Queries
+    template_name = "frkpapp/history.html"
+
+
+class ParametersUpdateView(UpdateView):
+    model = Queries
+    form_class = AddPostForm
+    template_name = 'frkpapp/param_update.html'
+    success_url = reverse_lazy('history')
+
+
+class ParametersDeleteView(DeleteView):
+    model = Queries
+    template_name = 'frkpapp/param_delete.html'
+    success_url = reverse_lazy('history')
+
+
+class ParametersSendView(TemplateView):
+    template_name = "frkpapp/param_send.html"
+
+    def get_context_data(self, **kwargs):
+        data = get_object_or_404(Queries, pk=self.kwargs['pk'])
+        data.status = 1
+        data.save(update_fields=['status'])
